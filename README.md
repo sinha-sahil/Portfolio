f33c978758feea597f45c0dd93a47509f5ffd8d3
Sahil_Sinha <sahil.sinha@juspay.in>
2019-07-05 19:55:44 +0530

    Completed SideBarView

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Portofolio
-This Project contains codebase for portfolio site of Sahil Sinha
+This Project contains codebase for portfolio site of Sahil Sinha.
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentPanelComponent } from './components/content-panel/content-panel.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContentPanelComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
